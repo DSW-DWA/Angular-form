fa1534f426d95e3e45bc6167634e82487beb69d9
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-05-06 20:54:51 +0500

    create auotcomplete form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+import apartments from './apartments.json'
+import { Apartment } from './interfaces'
+
+import houses from './houses.json'
+import { House } from './interfaces'
+
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  dom:boolean = false;
+export class AppComponent implements OnInit{
+
+  HOUSES: House[] = houses;
+
+  myControl = new FormControl();
+  options: Apartment[] = apartments;
+  filteredOptions!: Observable<Apartment[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.name)),
+      map(name => (name ? this._filter(name) : this.options.slice())),
+    );
+  }
+
+  private _filter(name: string): Apartment[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+  displayFn(Apart: Apartment): string {
+    return Apart && Apart.name ? Apart.name : '';
+  }
+  selectedValue = null;
   title = 'form';
+  show(){
+    console.log(this.selectedValue);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
-
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,11 @@ import {MatSelectModule} from '@angular/material/select';
     BrowserModule,
     NoopAnimationsModule,
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
