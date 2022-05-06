ff11d3b62f77b97d92550cde6f20f03ad2ae00aa
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-05-06 09:34:32 +0500

    create houses-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HouseFormComponent } from './house-form/house-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseFormComponent
   ],
   imports: [
     BrowserModule
