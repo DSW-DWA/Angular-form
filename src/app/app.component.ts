e2f21178ed6e009f94add6e1d4cf2c6025568c50
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-05-07 22:52:24 +0500

    renaming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import {AbstractControl, FormControl, ValidatorFn, Validators } from '@angular/f
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 
+//invalid error Autocompelte function
 function autocompleteObjectValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     if (typeof control.value === 'string') {
@@ -31,7 +32,7 @@ function autocompleteObjectValidator(): ValidatorFn {
 export class AppComponent implements OnInit{
 
   // initialization variables
-  idSelectHouse: number = 0;
+  idSelectApart: number = 0;
   HOUSES: House[] = houses;
   
   
@@ -44,13 +45,13 @@ export class AppComponent implements OnInit{
   controlApart = new FormControl('', { validators: [autocompleteObjectValidator(), Validators.required] });
   controlPerson = new FormControl('', { validators: [autocompleteObjectValidator(), Validators.required] });
 
-  selectedHouse:Apartment ={
+  selectedApart:Apartment ={
     id: -1,
     hid: -1,
     name:"",
     area:-1
   };
-  selectedMan: Person = {
+  selectedPerson: Person = {
     id: -1,
     aid: -1,
     name: ""
@@ -58,7 +59,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit() {
 
-    // create option array for first autocomplete
+    // create option array for apartment autocomplete
     this.optionsApart.forEach(item =>{
       let ind:number = this.optionsApart.indexOf(item);
       this.HOUSES.forEach(element =>{
@@ -66,7 +67,7 @@ export class AppComponent implements OnInit{
       })
     })
 
-    //filtered options
+    //filtered array for autocomplete
     this.filteredApart = this.controlApart.valueChanges.pipe(
       startWith(''),
       map(value => (typeof value === 'string' ? value : value.name)),
@@ -80,7 +81,7 @@ export class AppComponent implements OnInit{
 
   }
 
-  // filter
+  // filter for autocomplete
   private _filter(name: string): Apartment[] {
     const filterValue = name.toLowerCase();
 
@@ -93,33 +94,33 @@ export class AppComponent implements OnInit{
   }
 
   // displayFn for visualisation in mat-autocmoplete
-  displayFn(Apart: Apartment): string {
+  displayApart(Apart: Apartment): string {
     return Apart && Apart.name ? Apart.name : '';
   }
-  displayFn1(Apart: Person): string {
+  displayPerson(Apart: Person): string {
     return Apart && Apart.name ? Apart.name : '';
   }
 
   // change option for first autocomplete
   ChangeApart(){
     this.optionsApart.length = 0;
-    this.selectedHouse = {
+    this.selectedApart = {
       id: -1,
       hid: -1,
       name:"",
       area:-1
     }
-    this.selectedMan = {
+    this.selectedPerson = {
       id: -1,
       aid: -1,
       name: ""
     }
-    let NameSelectHouse: string;
+    let NameSelectApart: string;
     this.HOUSES.forEach(element =>{
-      if (element.id == this.idSelectHouse ) {NameSelectHouse = element.name; return}
+      if (element.id == this.idSelectApart ) {NameSelectApart = element.name; return}
     })
     apartments.forEach(item => {
-      if (item.hid == this.idSelectHouse) this.optionsApart.push(item);
+      if (item.hid == this.idSelectApart) this.optionsApart.push(item);
     });
     this.filteredApart = this.controlApart.valueChanges.pipe(
       startWith(''),
@@ -129,14 +130,14 @@ export class AppComponent implements OnInit{
   }
   // change option for second autocomplete
   ChangePerson(){
-    this.selectedMan = {
+    this.selectedPerson = {
       id: -1,
       aid: -1,
       name: ""
     }
     this.optionPerson.length = 0;
     persons.forEach(element=>{
-      if ( this.selectedHouse.id == -1 ||element.aid == this.selectedHouse.id) this.optionPerson.push(element);
+      if ( this.selectedApart.id == -1 ||element.aid == this.selectedApart.id) this.optionPerson.push(element);
     })
     this.filteredPerson = this.controlPerson.valueChanges.pipe(
       startWith(''),
