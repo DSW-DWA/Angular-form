6d4d7a4cac8267c7512ec6f0493e3e7c9dc1e19b
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-05-07 13:31:28 +0500

    create all imput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit} from '@angular/core';
 
-import apartments from './apartments.json'
-import { Apartment } from './interfaces'
+import apartments from './apartments.json';
+import { Apartment } from './interfaces';
 
-import houses from './houses.json'
-import { House } from './interfaces'
+import houses from './houses.json';
+import { House } from './interfaces';
+
+import persons from './persons.json';
+import { Person } from './interfaces';
+
 
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
@@ -22,11 +26,20 @@ export class AppComponent implements OnInit{
   myControl = new FormControl();
   options: Apartment[] = [...apartments];
   filteredOptions!: Observable<Apartment[]>;
+  selectedValue:Apartment ={
+    id: -1,
+    hid: -1,
+    name:"",
+    area:-1
+  };
+  optionForChild: Person[] = [...persons];
+
+  myControl1 = new FormControl();
+  filteredOptions1!: Observable<Person[]>;
 
   // change option for filter
   ChangeOpt(){
     this.options.length = 0;
-    let tmp!: House[];
     let NameSelectHouse: string;
     this.HOUSES.forEach(element =>{
       if (element.id == this.idSelectHouse ) {NameSelectHouse = element.name; return}
@@ -54,6 +67,11 @@ export class AppComponent implements OnInit{
       map(value => (typeof value === 'string' ? value : value.name)),
       map(name => (name ? this._filter(name) : this.options.slice())),
     );
+    this.filteredOptions1 = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.name)),
+      map(name => (name ? this._filter1(name) : this.optionForChild.slice())),
+    );
   }
   // filter
   private _filter(name: string): Apartment[] {
@@ -61,13 +79,27 @@ export class AppComponent implements OnInit{
 
     return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
   }
+  private _filter1(name: string): Person[] {
+    const filterValue = name.toLowerCase();
+
+    return this.optionForChild.filter(optionForChild => optionForChild.name.toLowerCase().includes(filterValue));
+  }
   // displayFn for visualisation in mat-autocmoplete
   displayFn(Apart: Apartment): string {
     return Apart && Apart.name ? Apart.name : '';
   }
-  selectedValue = null;
-  title = 'form';
+  displayFn1(Apart: Person): string {
+    return Apart && Apart.name ? Apart.name : '';
+  }
   show(){
-    console.log(this.selectedValue);
+    this.optionForChild.length = 0;
+    persons.forEach(element=>{
+      if ( this.selectedValue.id == -1 ||element.aid == this.selectedValue.id) this.optionForChild.push(element);
+    })
+    this.filteredOptions1 = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.name)),
+      map(name => (name ? this._filter1(name) : this.optionForChild.slice())),
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
-import { AutocompletePeopleComponent } from './autocomplete-people/autocomplete-people.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AutocompletePeopleComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
